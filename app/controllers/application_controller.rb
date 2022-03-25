96da79a700f2a3b6aaca49440bdedc298171a2cb
William Meyer <valodier@icloud.com>
2022-03-25 13:51:09 -0400

    added functionality to RESTful actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,49 @@
 class ApplicationController < ActionController::API
 
   def index 
-    
+    @users = User.all
+    render :index
   end
   
   def show
-    
+    user_id = params[:id]
+    @user = User.find_by(id: user_id)
+    render :show
   end
 
   def create
+    @user = User.create(
+      name: params["name"] 
+    )
+    if @user.save
+      render :show
+    else
+      render json: {errors: @user.error.full_messages},
+      status: 422
+    end
     
   end
 
   def update
+    user_id = params[:id]
+    @user = User.find_by(id: movie_id)
+
+    @user.name = params[:name] 
+
+    if user.save
+      render :show
+    else
+      render json: {errors: @user.error.full_messages},
+      status: 422
+    end
     
   end
 
   def destroy
-    
+    user_id = params[:id]
+    @user = User.find_by(id: user_id)
+    @user.destroy
+    render json: {message: "OH GOD NOT LIKE THIS"}
   end
 
 
