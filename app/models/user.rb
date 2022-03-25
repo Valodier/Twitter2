f2ddccf8a3d1d6b5db6a612ad3efb97c645a985c
William Meyer <valodier@icloud.com>
2022-03-25 16:06:29 -0400

    added associations to relationships and index

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :leader_relationships, class_name: "Relationships", foreign_key: "follower_id"
+  has_many :leaders, through :leader_relationships, source: :leader
+  has_many :follower_relationships, class_name: "Relationships", foreign_key: "leader_id"
+  has_many :followers, through :follower_relationships, source: :follower
+
 end
