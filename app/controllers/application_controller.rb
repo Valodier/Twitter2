4f5a5baf2ddc3009a34768b166cfbfd52a95ac5e
William Meyer <valodier@icloud.com>
2022-03-25 13:41:28 -0400

    Adds empty RESTful actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < ActionController::API
+
+  def index 
+    
+  end
   
+  def show
+    
+  end
+
+  def create
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+
 end
